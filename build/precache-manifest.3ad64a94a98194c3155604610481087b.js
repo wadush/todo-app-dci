8fb4c0d0d33514749fc2f18353b7ef8d00077842
DCI-REC109 <you@example.com>
2020-08-28 14:50:10 +0200

    test

---
+++ build/precache-manifest.3ad64a94a98194c3155604610481087b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f8dd4e6534458aaf021200b85daeb55d",
+    "url": "/todo-app-dci/index.html"
+  },
+  {
+    "revision": "c69f1cb91fd70415c3d1",
+    "url": "/todo-app-dci/static/css/main.b33c6990.chunk.css"
+  },
+  {
+    "revision": "9de1b746e355e5dd67b8",
+    "url": "/todo-app-dci/static/js/2.547f1ecc.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/todo-app-dci/static/js/2.547f1ecc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c69f1cb91fd70415c3d1",
+    "url": "/todo-app-dci/static/js/main.ece14e0d.chunk.js"
+  },
+  {
+    "revision": "7ff7fc5752aa65769379",
+    "url": "/todo-app-dci/static/js/runtime-main.3bb82477.js"
+  },
+  {
+    "revision": "d80fbbb9bf36d4a9318c0ba8963a236c",
+    "url": "/todo-app-dci/static/media/todo-illustration.d80fbbb9.jpg"
+  }
+]);
